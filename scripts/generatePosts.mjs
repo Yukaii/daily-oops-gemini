c1c6f05c0eecf5ed51967e4bf220c35ed0728a8a
Yukai Huang <yukaihuangtw@gmail.com>
2023-07-27 13:07:43 +0800

    feat: fix index generation

--- scripts/generatePosts.mjs
+++ scripts/generatePosts.mjs
@@ -9,7 +9,8 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const outputDir = path.join(__dirname, "../posts");
+const postsDir = path.join(__dirname, "../posts");
+const outputDir = path.join(__dirname, "../output");
 
 process.env.HACKMD_PROFILE;
 const apiUrl = `https://hackmd.io/api/@${process.env.HACKMD_PROFILE}/overview`;
@@ -20,18 +21,19 @@ async function run() {
   // save posts as markdown file
   await Promise.all(notes.map(async (post) => {
     const fullContent = await fetch(`https://hackmd.io/${post.id}/download`).then((res) => res.text());
-    fs.writeFileSync(path.join(outputDir, `${post.shortId}.md`), fullContent, 'utf8');
+    fs.writeFileSync(path.join(postsDir, `${post.shortId}.md`), fullContent, 'utf8');
   }));
 
   const indexContent = generateIndex(notes);
+  console.log(indexContent);
 
-  fs.writeFileSync(path.join(outputDir, `index.md`), indexContent, 'utf8');
+  fs.writeFileSync(path.join(outputDir, `index.gmi`), indexContent, 'utf8');
 }
 
 function generateIndex(posts) {
   const linkes = posts
     .map((post) => {
-      return `- [${post.title}](./${post.shortId}.gemtext)`;
+      return `=> ./${post.shortId}.gmi ${post.title}`;
     })
     .join("\n");
 
